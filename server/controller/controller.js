f511281f1d0a4229a32ca73f7dd333318b93b817
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-15 16:45:11 -0500

    Created a new user and save user in the database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -10,6 +10,26 @@ exports.create = (req,res) =>{
     }
 
 
+    // new user
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    // save user in the database
+    user
+    .save(user)
+    .then(data=>{
+        res.send(data)
+    })
+    .catch(err=>{
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating a create operation"
+        });
+    });
+
 }
 
 // retrieve and return all users / retrieve and return a single user
