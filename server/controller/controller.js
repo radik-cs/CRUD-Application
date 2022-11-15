15e8a411b0a0c52a8cf0edd16970a7aa966d5d78
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-15 16:37:53 -0500

    Sterted to work on Create function

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,5 +2,27 @@ var Userdb = require('../model/model');
 
 // create and save new user
 exports.create = (req,res) =>{
-    
+
+    // validate request
+    if(req.body){
+        res.status(400).send({message: "Content can not be empty!"});
+        return;
+    }
+
+
+}
+
+// retrieve and return all users / retrieve and return a single user
+exports.find = (req, res)=>{
+
+}
+
+// Update a new identified user by user id
+exports.update = (req, res)=>{
+
+}
+
+// Delete a user with specified user id in the request
+exports.delete = (req, res)=>{
+
 }
