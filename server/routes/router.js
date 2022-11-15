ca4691a2e8f96442deb84364e596497b7918659e
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-15 16:10:12 -0500

    Connected MongoDB

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,14 +6,26 @@ const services = require('../services/render');
 
 
 /**
- * @description Root Route
+ * @description for Root Route
  * @method GET /
  */
 
 route.get('/',services.homeRoutes);
     
+
+/**
+ * @description for add user
+ * @method GET / add-user
+ */
+
 route.get('/add-user',services.add_user);
     
+
+/**
+ * @description for  update user
+ * @method GET / update-user
+ */
+
 route.get('/update-user',services.update_user);
 
 module.exports=route
