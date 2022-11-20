b87605122e3455658d500291680893b2793058cf
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-20 16:19:59 -0500

    Worked with API. Functions to get all the details from DB and display them

--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,23 @@
+const axios = require("axios");
+
 exports.homeRoutes = (req,res) =>{
-    res.render('index');
+    res.render('index', {users: "New Data"});
 }
 
 
 exports.add_user = (req, res) =>{
+
+    // Make a Get request to /api/users
+    axios.get('https://localhost:3000/api/users')
+    .then(function(response){
+      
+        res.render('index', {users: response.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
+
+
     res.render('add_user');
 }
 
