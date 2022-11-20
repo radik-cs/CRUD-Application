914aabdef49aaaca00a411de805fb7f5105954c8
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-20 15:16:28 -0500

    Completed the FindAndUpdate method

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -47,6 +47,26 @@ exports.find = (req, res)=>{
 
 // Update a new identified user by user id
 exports.update = (req, res)=>{
+    if(!req.body){
+        return res
+        .status(400)
+        .send({message:"Data to update can not be empty"})
+    }
+
+    const id = rew.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify:false})
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:`Cannot Update user with ${id}. Maybe user not found!`})
+        }else{
+            res.send(data)
+        }
+
+    })
+    .catch(err=>{
+        res.status(500).send({message:"Error Update user information"})
+    })
+
 
 }
 
