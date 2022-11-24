8937cfe6f51ce460ea4b4bc0107a29656b6496c7
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-24 01:28:46 -0500

    Worked on Delete User: How to Delete a Specific Record from DB

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,6 +30,27 @@ $("#update_user").submit(function(event){
 
     $.ajax(request).done(function(response){
         alert("Data Updated Successfully!");
-
     })
 })
+
+if(window.location.pathname == "/"){
+    $ondelete =$(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id =$(this).attr("data-id")
+
+        var request = {
+            "url":`https://localhost:3000/api/users/${id}`,
+            "method":"DELETE",
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload()
+            })
+
+        }
+
+
+    })
+}
