6914e55205ae6f75f682d30355f3d6a0e646b895
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-24 02:05:03 -0500

    Modified some of the files

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config({path:'config.env'})
-
+dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
 // log requests
@@ -19,21 +18,18 @@ app.use(morgan('tiny'));
 connectDB();
 
 // parse request to body-parser
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
-app.set("view engine","ejs")
-// app.set("views", path.resolve(__dirname, "views/ejs"))
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-
-
 // load routers
 app.use('/', require('./server/routes/router'))
 
-
-app.listen(PORT,()  => {console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
