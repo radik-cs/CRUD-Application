0554bb9a4bb7bebbc6196d7324c3bb2edbbb82cd
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-24 01:44:35 -0500

    Fixed some errors

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,30 +4,24 @@ const route = express.Router()
 const services = require('../services/render');
 const controller = require('../controller/controller');
 
-
+/**
+ *  @description Root Route
+ *  @method GET /
+ */
+route.get('/', services.homeRoutes);
 
 /**
- * @description for Root Route
- * @method GET /
+ *  @description add users
+ *  @method GET /add-user
  */
-
-route.get('/',services.homeRoutes);
-    
+route.get('/add-user', services.add_user)
 
 /**
- * @description for add user
- * @method GET / add-user
+ *  @description for update user
+ *  @method GET /update-user
  */
+route.get('/update-user', services.update_user)
 
-route.get('/add-user',services.add_user);
-    
-
-/**
- * @description for  update user
- * @method GET / update-user
- */
-
-route.get('/update-user',services.update_user);
 
 // API
 route.post('/api/users', controller.create);
@@ -35,4 +29,5 @@ route.get('/api/users', controller.find);
 route.put('/api/users/:id', controller.update);
 route.delete('/api/users/:id', controller.delete);
 
-module.exports=route
+
+module.exports = route
