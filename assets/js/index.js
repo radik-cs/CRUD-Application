3281d5174c48d86d809fe6b70a419649f89da54f
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-11-24 00:56:58 -0500

    Working on Update User

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,18 @@
 $("#add_user").submit(function(event){
     alert("Data Inserted Succesfully");
 })
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n,i){
+        data[n['name']] =n['value']
+
+
+    })
+
+    console.log(data);
+})
